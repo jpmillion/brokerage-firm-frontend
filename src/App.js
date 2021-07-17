eeb748430f6d6676c0cb7045e9e88ee666af6850
john million <jpmillion@comcast.net>
2021-07-17 00:27:12 -0400

    change file path of login component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Login from './presentational/Login';
+import Login from './features/user/Login';
 import './App.css';
 
 function App() {
