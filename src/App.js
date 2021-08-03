75f3f006f2ac2d4b3f2f9c8931b88aeb2256f4f9
john million <jpmillion@comcast.net>
2021-08-03 19:57:54 -0400

    Define useEffect Hook in App.js to authenticate user on page refresh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './features/user/Login';
 import './App.css';
+import { authenticate } from './features/user/userAPI';
 
 function App() {
+  useEffect(() => {
+    if (sessionStorage.token) authenticate();
+  })
+
   return (
     <div>
       <Router>
