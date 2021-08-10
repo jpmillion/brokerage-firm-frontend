60e35b7f89bd75dfe3a7535ecc1ca00a30b61d39
john million <jpmillion@comcast.net>
2021-08-09 20:10:34 -0400

    Refractor App.js to display logout button when logged in

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import React, { useEffect } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './features/user/Login';
 import './App.css';
-import { authenticate } from './features/user/userAPI';
+import { useDispatch, useSelector } from 'react-redux';
+import { asyncAuthenticate, logOut, selectLogInStatus } from './features/user/userSlice';
+import LogoutGoogle from './features/user/LogoutGoogle';
 
 function App() {
+  const dispatch = useDispatch();
+  const loggedIn = useSelector(selectLogInStatus);
+
+  const handleLogOut = () => {
+    sessionStorage.clear();
+    dispatch(logOut());
+  }
+
+  const logOutButton = <button onClick={handleLogOut} >Log Out</button>;
+  
+  const displayLogOutButton = () => {
+    if (loggedIn) {
+      return !!sessionStorage.token ? logOutButton : <LogoutGoogle />;
+    }
+  }
+
   useEffect(() => {
-    if (sessionStorage.token) authenticate();
+    if (sessionStorage.token) dispatch(asyncAuthenticate());
   })
 
   return (
     <div>
+      {displayLogOutButton()}
       <Router>
           <Route path='/login' component={Login} />
       </Router>
